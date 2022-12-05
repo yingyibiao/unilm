60ce4c227da9f2a8d4d1a95e6cf104a52241b64c
ying15 <yibiao.ying@learnable.ai>
2022-12-05 02:27:16 +0000

    add proxy for github

--- layoutlmft/layoutlmft/data/datasets/xfun.py
+++ layoutlmft/layoutlmft/data/datasets/xfun.py
@@ -10,7 +10,7 @@ from transformers import AutoTokenizer
 
 
 _URL = "https://github.com/doc-analysis/XFUN/releases/download/v1.0/"
-
+_URL = "https://ghproxy.com/https://github.com/doc-analysis/XFUN/releases/download/v1.0/"
 _LANG = ["zh", "de", "es", "fr", "en", "it", "ja", "pt"]
 logger = logging.getLogger(__name__)
 
